0e1892b9f6dc15701572031edbbd9fe7adc7b3a1
Pi Delport <pjdelport@gmail.com>
2019-04-01 23:05:54 +0200

    Include MDC components: floating-label, textfield

--- build-assets.sh
+++ build-assets.sh
@@ -5,6 +5,6 @@ mkdir -p build/assets
 
 cp -anv node_modules/normalize.css/normalize.css -t build/assets
 
-for package in button card layout-grid ripple theme typography; do
+for package in button card floating-label layout-grid ripple textfield theme typography; do
     cp -anv "node_modules/@material/$package/dist/"*.min.* -t build/assets
 done
