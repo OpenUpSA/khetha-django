a66e030831527d8e94964324e19a65a981de0c66
Pi Delport <pjdelport@gmail.com>
2019-04-01 02:06:46 +0200

    Add TaskDetailView

--- src/khetha/views.py
+++ src/khetha/views.py
@@ -5,3 +5,7 @@ from khetha import models
 
 class TaskListView(generic.ListView):
     queryset = models.Task.objects.filter(is_published=True)
+
+
+class TaskDetailView(generic.DetailView):
+    queryset = models.Task.objects.filter(is_published=True)
--- tests/test_views.py
+++ tests/test_views.py
@@ -6,6 +6,17 @@ from django.urls import reverse
 
 from khetha import models
 
+# Test helpers:
+# (Check the counts, for test integrity.)
+
+
+def _publish_tasks() -> None:
+    assert 0 < models.Task.objects.update(is_published=True)
+
+
+def _unpublish_tasks() -> None:
+    assert 0 < models.Task.objects.update(is_published=False)
+
 
 class TestTaskListView(TestCase):
     fixtures = ["sample-task-data"]
@@ -22,9 +33,7 @@ class TestTaskListView(TestCase):
         self.assertQuerysetEqual([], response.context["task_list"])
 
     def test_get__published(self) -> None:
-        count = models.Task.objects.update(is_published=True)
-        assert 0 < count  # Check test integrity
-
+        _publish_tasks()
         response = self._get()
         expected_tasks = models.Task.objects.all()
         self.assertQuerysetEqual(
@@ -36,3 +45,34 @@ class TestTaskListView(TestCase):
         for task in expected_tasks:
             with self.subTest(task=task):
                 self.assertContains(response, task.title)
+
+
+class TestTaskDetailView(TestCase):
+    fixtures = ["sample-task-data"]
+
+    def _get(self, *, slug: str) -> HttpResponse:
+        return self.client.get(reverse("task-detail", kwargs={"slug": slug}))
+
+    def test_not_found(self) -> None:
+        _unpublish_tasks()
+        task: models.Task = models.Task.objects.first()
+        response = self._get(slug=task.slug)
+        assert HTTPStatus.NOT_FOUND == response.status_code
+        self.assertTemplateNotUsed("khetha/task_detail.html")
+        assert "task" not in response.context
+
+    def _get_ok(self, task: models.Task) -> HttpResponse:
+        response = self._get(slug=task.slug)
+        assert HTTPStatus.OK == response.status_code
+        self.assertTemplateUsed("khetha/task_detail.html")
+        assert task == response.context["task"]
+        self.assertContains(response, task.title, count=1)
+        for question in task.question_set.all():
+            self.assertContains(response, question.text, count=1)
+        return response
+
+    def test_get__published(self) -> None:
+        _publish_tasks()
+        for task in models.Task.objects.all():
+            with self.subTest(task=task):
+                self._get_ok(task)
