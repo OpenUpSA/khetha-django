86c610b37aced12f6d619d4738962344a2db2f11
Pi Delport <pjdelport@gmail.com>
2019-04-03 16:18:15 +0200

    Update task list URL path: task -> tasks
    This follows convention better.

--- src/khetha/urls.py
+++ src/khetha/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from khetha import views
 
 urlpatterns: list = [
-    path("", views.TaskListView.as_view(), name="task-list"),
+    path("tasks/", views.TaskListView.as_view(), name="task-list"),
     path("tasks/<slug:slug>/", views.TaskDetailView.as_view(), name="task-detail"),
     path("admin/", admin.site.urls),
 ]
