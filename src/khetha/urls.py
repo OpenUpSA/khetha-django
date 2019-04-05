16cc03d580ee225291293eae15169aba705a5f83
Pi Delport <pjdelport@gmail.com>
2019-04-05 14:58:11 +0200

    (Comments)

--- src/khetha/urls.py
+++ src/khetha/urls.py
@@ -5,8 +5,10 @@ from django.views import generic
 from khetha import views
 
 urlpatterns: list = [
+    # Khetha:
     path("", generic.RedirectView.as_view(pattern_name="task-list"), name="home"),
     path("tasks/", views.TaskListView.as_view(), name="task-list"),
     path("tasks/<slug:slug>/", views.TaskDetailView.as_view(), name="task-detail"),
+    # Django admin:
     path("admin/", admin.site.urls),
 ]
