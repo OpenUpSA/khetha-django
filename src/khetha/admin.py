f0759fcb4d20ab3cb20fa47a16599d4ef8aea519
Pi Delport <pjdelport@gmail.com>
2019-04-05 22:39:01 +0200

    Admin: Add TaskSubmission and Answer

--- src/khetha/admin.py
+++ src/khetha/admin.py
@@ -1,7 +1,9 @@
 from adminsortable2.admin import SortableAdminMixin, SortableInlineAdminMixin
+from django import forms
 from django.contrib import admin
 from django.contrib.admin.options import InlineModelAdmin
 from django.contrib.auth.admin import UserAdmin as django_UserAdmin
+from django.db.models.fields import TextField
 
 from khetha import models
 
@@ -35,6 +37,13 @@ class AnswerOptionInline(SortableInlineAdminMixin, _TabularInline):
     raw_id_fields = ["question"]
 
 
+class AnswerInline(_TabularInline):
+    model = models.Answer
+    ordering = ["question__order"]
+    raw_id_fields = ["tasksubmission", "question"]
+    formfield_overrides = {TextField: {"widget": forms.TextInput}}
+
+
 @admin.register(models.Task)
 class TaskAdmin(SortableAdminMixin, admin.ModelAdmin):
     list_display = ["slug", "title", "description", "points", "is_published"]
@@ -57,3 +66,18 @@ class QuestionAdmin(admin.ModelAdmin):
 class AnswerOptionAdmin(admin.ModelAdmin):
     list_display = ["text", "question"]
     raw_id_fields = ["question"]
+
+
+@admin.register(models.TaskSubmission)
+class TaskSubmissionAdmin(admin.ModelAdmin):
+    date_hierarchy = "modified_at"
+    ordering = ["-modified_at"]
+    list_display_links = ["task", "user_key"]
+    list_display = ["task", "user_key", "modified_at"]
+    search_fields = ["task__title", "user_key"]
+    list_filter = [("task", admin.RelatedOnlyFieldListFilter)]
+
+    raw_id_fields = ["task"]
+    readonly_fields = ["created_at", "modified_at"]
+
+    inlines = [AnswerInline]
