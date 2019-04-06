2b09eb74e6758b5ed6452049d26919ed704f9237
Pi Delport <pjdelport@gmail.com>
2019-04-06 23:45:15 +0200

    Disable use_scm_version, for GitLab testing
    (We don't really need the versioning until we start releasing to PyPI or
    similar, and omitting this simplifies CI/CD requirements.)

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     # use_scm_version still needs to be setup.py rather than setup.cfg.
     # See: https://github.com/pypa/setuptools_scm/#setupcfg
-    use_scm_version=True
+    # use_scm_version=True
 )
