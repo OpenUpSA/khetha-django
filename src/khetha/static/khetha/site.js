84d5d5ec33657a838baa0a1f94ac0e283c162908
Pi Delport <pjdelport@gmail.com>
2019-04-08 19:17:48 +0200

    Switch MDC initialisation over to use auto-init
    This makes things easier down the line, and enables getting to a DOM
    node's component in a simple and reliable way.

--- src/khetha/static/khetha/site.js
+++ src/khetha/static/khetha/site.js
@@ -1,28 +1,22 @@
 /* Site-wide initialisation for Khetha. */
 
 function initMDC() {
-    // https://material.io/develop/web/components/top-app-bar/#javascript-instantiation
-    const topAppBar = new window.mdc.topAppBar.MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
+  // https://material.io/develop/web/components/auto-init/
+  window.mdc.autoInit(
+    document,
+    // This initialisation gets called more than once per page load,
+    // so don't warn for already-initialised components.
+    function() {}
+  );
 
-    //https://material.io/develop/web/components/input-controls/text-field/#javascript-instantiation
-    const textFields= [].map.call(document.querySelectorAll('.mdc-text-field'), function (el) {
-        return new window.mdc.textfield.MDCTextField(el);
-    });
-
-    // https://material.io/develop/web/components/cards/#javascript
-    const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
-    const ripples = [].map.call(document.querySelectorAll(selector), function (el) {
-        return new window.mdc.ripple.MDCRipple(el);
-    });
-
-    // https://material.io/develop/web/components/snackbars/#javascript-instantiation
-    const snackbars = [].map.call(document.querySelectorAll('.mdc-snackbar'), function (el) {
-        console.log(el);
-        let snackbar = new window.mdc.snackbar.MDCSnackbar(el);
-        // The Django messages snackbars should open on page load.
-        snackbar.open();
-        return snackbar;
-    });
+  // https://material.io/develop/web/components/snackbars/#javascript-instantiation
+  [].map.call(document.querySelectorAll(".mdc-snackbar"), function(el) {
+    if (el.MDCSnackbar) {
+      el.MDCSnackbar.open();
+    } else {
+      console.log("Warning, MDCSnackbar not initialised:", el);
+    }
+  });
 }
 
 initMDC();
