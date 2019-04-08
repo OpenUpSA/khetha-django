17a68f60492e6610c8f6dfc38f4d53aba22babe3
Pi Delport <pjdelport@gmail.com>
2019-04-08 19:15:20 +0200

    Switch over to using combined material-components-web bundle
    The site includes enough of it now that partially including modules does
    not save meaningful resources.

--- build-assets.sh
+++ build-assets.sh
@@ -4,20 +4,5 @@ test -e build/assets && rm -r build/assets
 mkdir -p build/assets
 
 cp -av node_modules/normalize.css/normalize.css build/assets
-
-for package in \
-    button \
-    card \
-    floating-label \
-    layout-grid \
-    ripple \
-    snackbar \
-    textfield \
-    theme \
-    top-app-bar \
-    typography \
-; do
-    cp -av "node_modules/@material/$package/dist/"*.min.* build/assets
-done
-
+cp -av node_modules/material-components-web/dist/material-components-web.min.* build/assets
 cp -av node_modules/zepto/dist/zepto.min.js build/assets
