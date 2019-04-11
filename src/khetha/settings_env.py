715c77caec4f4e8b0e1c2572b727f4cd737cbc12
Pi Delport <pjdelport@gmail.com>
2019-04-11 19:48:37 +0200

    Settings: Push STATICFILES_DIRS into environment (where it should be)

--- src/khetha/settings_env.py
+++ src/khetha/settings_env.py
@@ -19,6 +19,25 @@ if "DJANGO_STATIC_ROOT" in env:  # pragma: no cover
 if "DJANGO_ALLOWED_HOSTS" in env:  # pragma: no cover
     ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
 
+if "DJANGO_STATICFILES_DIRS" in env:  # pragma: no cover
+    # XXX (Pi): Move this into django-environ-base.
+    #
+    # This:
+    #
+    #   DJANGO_STATICFILES_DIRS=node_modules,assets:build/assets
+    #
+    # Will yield this:
+    #
+    #   STATICFILES_DIRS = [
+    #       'node_modules',
+    #       ("assets", "build/assets"),
+    #   ]
+    #
+    STATICFILES_DIRS = env.list(
+        "DJANGO_STATICFILES_DIRS",
+        cast=lambda s: (tuple(s.split(":")) if ":" in s else s),
+    )
+
 if "GOOGLE_MAPS_API_KEY" in env:  # pragma: no cover
     GOOGLE_MAPS_API_KEY = env("GOOGLE_MAPS_API_KEY")
 
@@ -71,11 +90,6 @@ AUTH_USER_MODEL = "khetha.User"
 # Time zone (hard-coded, for now)
 USE_TZ = True
 TIME_ZONE = "Africa/Johannesburg"
-
-
-# Tell collectstatic where to find the static assets collected by build-assets.sh
-# (XXX: This should go in dev/build config.)
-STATICFILES_DIRS = [("assets", "build/assets")]
 
 
 # Khetha relies on session persistence to identify people:
